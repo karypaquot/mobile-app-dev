ae93fb894fc673d49c96a76e53e31ec9ba708540
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-25 10:26:45 -0700

    Implementatioin
    added dependencies to the gradle
    photo gallery fragment layout
    photo galery fragment kotlin class

---
+++ lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.photogallery
+
+import androidx.fragment.app.Fragment
+
+class PhotoGalleryFragment: Fragment() {
+    private var _binding: FragmentPhotoGalleryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            
+        }
+}
