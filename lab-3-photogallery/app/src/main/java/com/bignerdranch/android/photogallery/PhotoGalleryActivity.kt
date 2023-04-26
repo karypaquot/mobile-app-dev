21a4c0c9df7665b7b519f12841a672af94648c20
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-26 12:11:18 -0700

    deleted

--- lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.bignerdranch.android.photogallery
-
-import android.app.Activity
-
-class PhotoGalleryActivity: Activity() {
-}
--- lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.photogallery.api
-
-import retrofit2.http.GET
-
-interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
-}
