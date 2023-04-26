f06c2828f6097232b1b49d44c830743a315f0b2a
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-26 10:35:05 -0700

    fixing gradle

--- lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,13 +1,22 @@
 package com.bignerdranch.android.photogallery
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bignerdranch.android.photogallery.api.FlickrApi
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
 
+private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment: Fragment() {
     private var _binding: FragmentPhotoGalleryBinding? = null
     private val binding
@@ -21,5 +30,23 @@ class PhotoGalleryFragment: Fragment() {
         _binding = FragmentPhotoGalleryBinding.inflate(inflater, container, false)
         binding.photoGrid.layoutManager = GridLayoutManager(context, 3)
         return binding.root
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        val flickrApi: FlickrApi = retrofit.create<FlickrApi>()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val response = flickrApi.fetchContents()
+            Log.d(TAG, "Response received: $response")
+        }
+
     }
 }
