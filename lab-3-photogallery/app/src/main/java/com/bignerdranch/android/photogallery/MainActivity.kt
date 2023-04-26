52e4fb5a824c7bd0426163e3882a7ec0d24a4fb1
Karina Hernandez <Karina.Hernandez08@student.csulb.edu>
2023-04-26 10:42:06 -0700

    Update MainActivity.kt

--- lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
+++ lab-3-photogallery/app/src/main/java/com/bignerdranch/android/photogallery/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.bignerdranch.android.photogallery
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 class MainActivity: AppCompatActivity() {
-    onCreateView()
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context,MainActivity::class.java)
+        }
+    }
 }
